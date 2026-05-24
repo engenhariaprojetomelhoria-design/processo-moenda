a5b186bd58648629bf5afb76477b400240254439
engenhariaprojetomelhoria-design <engenharia.projetomelhoria@gmail.com>
2026-05-24 13:40:39 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,17 +3,28 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from
 import { getFirestore, doc, getDoc, setDoc, collection, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMEJ-rXYUzSJicpI6cbVJHFPKPR8oQPIY",
+  apiKey: "SUA_API_KEY",
   authDomain: "processo-moenda.firebaseapp.com",
   projectId: "processo-moenda",
   storageBucket: "processo-moenda.firebasestorage.app",
   messagingSenderId: "746536211765",
   appId: "1:746536211765:web:3f0624d018f23227b6a87c"
 };
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, onAuthStateChanged, signInWithEmailAndPassword, signOut, doc, getDoc, setDoc, collection, getDocs, serverTimestamp };
+export {
+  auth,
+  db,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  serverTimestamp
+};
